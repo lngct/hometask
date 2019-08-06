917a3925f6d155413991989eab3e75c68eab02dd
lngct <4ever_usa@mail.ru>
2019-08-07 00:17:47 +0300

    отформатировал код

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 function PageHeader()
 {
-?>
+    ?>
     <header class="navbar navbar-dark bg-dark navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
@@ -10,50 +10,55 @@ function PageHeader()
             </div>
         </div>
     </header>
-<?php
+    <?php
 }
 
 function InputForm()
 {
-?>
+    ?>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9 col-lg-7 col-xl-6 pt-md-3">
                 <form method="post">
                     <div class="form-group">
                         <label for="clientname">Ваше имя</label>
-                        <input required type="text" class="form-control form-control-sm" name="clientName" id="clientname" placeholder="Игорь">
+                        <input required type="text" class="form-control form-control-sm" name="clientName"
+                               id="clientname" placeholder="Игорь">
                     </div>
                     <div class="form-group">
                         <label for="clientsurname">Ваша фамилия</label>
-                        <input required type="text" class="form-control form-control-sm" name="clientSurname" id="clientsurname" placeholder="Крупицын">
+                        <input required type="text" class="form-control form-control-sm" name="clientSurname"
+                               id="clientsurname" placeholder="Крупицын">
                     </div>
                     <div class="form-group">
                         <label for="clientphone">Ваш телефон</label>
-                        <input required type="tel" class="form-control form-control-sm" name="clientPhone" id="clientphone" placeholder="+7(000)000-00-00">
+                        <input required type="tel" class="form-control form-control-sm" name="clientPhone"
+                               id="clientphone" placeholder="+7(000)000-00-00">
                     </div>
                     <div class="form-group">
                         <label for="rates">Выберите тариф</label>
-                    <select class="form-control form-control-sm" id="rates">
-                        <option name="option1">var1</option>
-                        <option name="option2">var2</option>
-                        <option name="option3">var3</option>
-                    </select>
+                        <select class="form-control form-control-sm" id="rates">
+                            <option name="option1">var1</option>
+                            <option name="option2">var2</option>
+                            <option name="option3">var3</option>
+                        </select>
                     </div>
                     <div class="form-group">
                         <label for="selectdate">Укажите первый день, удобный для доставки</label>
-                        <input required type="date" name="selectdate" id="selectDate" class="form-control form-control-sm" id="">
+                        <input required type="date" name="selectdate" id="selectDate"
+                               class="form-control form-control-sm" id="">
                     </div>
                     <div class="form-group">
                         <label for="address">Укажите адрес доставки</label>
-                        <input required type="text" class="form-control form-control-sm" name="address" id="address" placeholder="г. Нижний тагил, ул. Цюрупы, 15">
+                        <input required type="text" class="form-control form-control-sm" name="address" id="address"
+                               placeholder="г. Нижний тагил, ул. Цюрупы, 15">
                     </div>
                     <button type="submit" class="btn btn-primary">Подтвердить</button>
                 </form>
             </div>
         </div>
     </div>
-<?php 
+    <?php
 }
 
 function DropDB()
@@ -62,34 +67,25 @@ function DropDB()
 
     // drop table Clients
 
-    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`Clients` ;") === TRUE)
-    {
+    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`Clients` ;") === TRUE) {
         print "Таблица Clients удалена<br>";
-    }
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
     // drop table rates
 
-    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`rates` ;") === TRUE)
-    {
+    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`rates` ;") === TRUE) {
         print "Таблица rates удалена<br>";
-    }
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
     // drop table orders
 
-    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`Orders` ;") === TRUE)
-    {
+    if (mysqli_query($db, "DROP TABLE IF EXISTS `hometask`.`Orders` ;") === TRUE) {
         print "Таблица Orders удалена<br>";
-    }
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
@@ -97,9 +93,9 @@ function DropDB()
 
 function InitDB()
 {
-	global $db;
+    global $db;
 
-	// Создание таблицы Clients
+    // Создание таблицы Clients
 
     if (mysqli_query($db, "CREATE TABLE IF NOT EXISTS `hometask`.`Clients` (
                   `clnt_id` INT NOT NULL AUTO_INCREMENT,
@@ -108,17 +104,13 @@ function InitDB()
                   `clnt_phone` VARCHAR(20) NOT NULL,
                   PRIMARY KEY (`clnt_id`),
                   UNIQUE INDEX `clientphone_UNIQUE` (`clnt_phone` ASC) VISIBLE)
-                ENGINE = InnoDB;") === TRUE)
-    {
+                ENGINE = InnoDB;") === TRUE) {
         print "Таблица Clients создана<br>";
-    }
-
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
-        // Создание таблицы rates
+    // Создание таблицы rates
 
     if (mysqli_query($db, "CREATE TABLE IF NOT EXISTS `hometask`.`rates` (
                   `rate_id` INT NOT NULL AUTO_INCREMENT,
@@ -126,17 +118,13 @@ function InitDB()
                   `rate_cost` DECIMAL(18,2) NOT NULL,
                   `dlvr_days` INT NOT NULL,
                   PRIMARY KEY (`rate_id`))
-                ENGINE = InnoDB;") === TRUE)
-    {
+                ENGINE = InnoDB;") === TRUE) {
         print "Таблица rates создана<br>";
-    }
-
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
-        // Создание таблицы Orders
+    // Создание таблицы Orders
 
     if (mysqli_query($db, "CREATE TABLE IF NOT EXISTS `hometask`.`Orders` (
                   `order_id` INT NOT NULL AUTO_INCREMENT,
@@ -157,13 +145,9 @@ function InitDB()
                     REFERENCES `hometask`.`rates` (`rate_id`)
                     ON DELETE NO ACTION
                     ON UPDATE NO ACTION)
-                ENGINE = InnoDB;") === TRUE)
-    {
+                ENGINE = InnoDB;") === TRUE) {
         print "Таблица Orders создана<br>";
-    }
-
-    else
-    {
+    } else {
         printf("Ошибка: %s\n", mysqli_error($db));
     }
 
@@ -179,13 +163,9 @@ function ClientsCheck()
 
     if (mysqli_query($db, "INSERT 
 INTO `Clients` (`clnt_name`, `clnt_surname`, `clnt_phone`) 
-VALUES ('$clientname', '$clientsurname', '$clientphone');") === TRUE)
-    {
+VALUES ('$clientname', '$clientsurname', '$clientphone');") === TRUE) {
         print "Запись в таблицу Clients добавлена<br>";
-    }
-
-    else
-    {
+    } else {
         $query = mysqli_query($db, "SELECT clnt_id FROM Clients WHERE clnt_phone LIKE '$clientphone'");
         $array = mysqli_fetch_array($query);
         mysqli_query($db, "UPDATE Clients SET clnt_name='$clientname', clnt_surname='$clientsurname' WHERE clnt_id='$array[0]'");
